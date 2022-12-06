ab2821817ebca84c47802d89cb6bef0dd840a5b0
Juan Esteban Berrío <juanestebanberrio@gmail.com>
2022-12-06 16:54:30 -0500

    firts commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Note;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        User::factory()->create([
+           'email' => 'desarrollo@albertoalvarez.com',
+           'password' => bcrypt('123456')
+        ]);
+
+        Note::factory(30)->create();
+    }
+}
